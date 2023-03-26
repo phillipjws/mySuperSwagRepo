e94ea8ff72c8238c7ea3de362067efb6d09ca82a
phillipjws <113144666+phillipjws@users.noreply.github.com>
2023-03-25 18:14:10 -0600

    assignment 1 completed

--- assignment1/A1Exercises.java
+++ assignment1/A1Exercises.java
@@ -8,7 +8,7 @@
  *  - DO NOT use builtin java Arrays methods
  */
 public class A1Exercises {
-	
+
 	/*
 	 * isFactor
 	 * Purpose: Determine if x can be evenly divided by y
@@ -22,13 +22,13 @@ public class A1Exercises {
 		} else {
 			return false;
 		}
-	}	
-	
+	}
+
 	/*
 	 * calcPower
 	 * Purpose: Calculate the value of base^exp
 	 * Parameters: int base - the base value
-	 *             int exp - the exponent value
+	 * int exp - the exponent value
 	 * Returns: int - the value of base^exp
 	 * Pre-conditions: base > 0, exp >= 0
 	 */
@@ -39,7 +39,7 @@ public class A1Exercises {
 		}
 		return result;
 	}
-	
+
 	/*
 	 * numFactors
 	 * Purpose: Determine the number of factors of n
@@ -50,9 +50,15 @@ public class A1Exercises {
 	public static int numFactors(int n) {
 		int result = 0;
 		// TODO: add code to finish the method implementation
+		result++; // n is always a factor of itself
+		for (int i = n / 2; i > 0; i--) {
+			if (n % i == 0) {
+				result++;
+			}
+		}
 		return result;
 	}
-	
+
 	/*
 	 * isPrime
 	 * Purpose: determine if n is a prime number
@@ -60,13 +66,20 @@ public class A1Exercises {
 	 * Returns: boolean - true if n is prime, false otherwise
 	 * Pre-conditions: n > 1
 	 *
-	 * Note: A prime number is a whole number greater 
+	 * Note: A prime number is a whole number greater
 	 * than 1 whose only factors are 1 and itself
 	 *
 	 * HINT: This method can be completed in 1 line.
 	 */
 	// TODO: write the isPrime method here
-	
+	public static boolean isPrime(int n) {
+		if (numFactors(n) <= 2) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	/*
 	 * greatestCommonDenominator
 	 * Purpose: determine the largest integer that divides x and y
@@ -75,52 +88,86 @@ public class A1Exercises {
 	 * Pre-conditions: x, y > 0
 	 */
 	// TODO: write the greatestCommonDenominator method here
-	
+	public static int greatestCommonDenominator(int x, int y) {
+		int gcd = 1;
+
+		for (int i = 1; i <= x && i <= y; i++) {
+			if (x % i == 0 && y % i == 0) {
+				gcd = i;
+			}
+		}
+		return gcd;
+	}
+
 	/*
 	 * printArray
 	 * Purpose: prints all values in the array to the console
-	 *          Example format:  {1,2,3,4}
+	 * Example format: {1,2,3,4}
 	 * Parameters: int[] - an array of integers
 	 * Returns: void - nothing
 	 */
-	public static void printArray (int[] array) {
+	public static void printArray(int[] array) {
 		System.out.print("{");
-		for(int i=0; i<array.length; i++) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]);
-			if(i<array.length-1) {
+			if (i < array.length - 1) {
 				System.out.print(",");
-			}	
+			}
 		}
 		System.out.println("}");
 	}
-	
+
 	/*
 	 * sumArray
-  	 * Purpose: computes the sum of all values in the given array
- 	 * Parameters: int[] array - the array of integers
+	 * Purpose: computes the sum of all values in the given array
+	 * Parameters: int[] array - the array of integers
 	 * Returns: int - sum of all values in the array
 	 * Pre-conditions: the array is not null (but could be empty)
 	 */
 	// TODO: write the sumArray method here
-	
+	public static int sumArray(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return sum;
+	}
+
 	/*
 	 * contains
 	 * Purpose: determines whether the array contains x
 	 * Parameters: int[] array - array of integers to search through
-	 *             int x - the value to search for
+	 * int x - the value to search for
 	 * Returns: boolean - true if value is found, false otherwise
 	 * Pre-conditions: the array is not null (but could be empty)
 	 */
 	// TODO: write the contains method here
-	
+	public static boolean contains(int[] array, int x) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == x) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/*
 	 * countMatches
 	 * Purpose: Determines the number of occurences of x in the given array
 	 * Parameters: int[] array - array of integers to search through
-	 *             int x - the value to search for
+	 * int x - the value to search for
 	 * Returns: int - the number of times x is found in the array
 	 * Pre-conditions: the array is not null (but could be empty)
 	 */
 	// TODO: write the countMatches method here */
-	
+	public static int countMatches(int[] array, int x) {
+		int matches = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == x) {
+				matches++;
+			}
+		}
+		return matches;
+	}
+
 }
